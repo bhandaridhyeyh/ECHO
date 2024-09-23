f4aebbe2f57121b0894448e0cbf09a5c1ba6d82e
Dhyey Bhandari <bhandaridhyeyh@gmail.com>
2024-09-23 11:29:26 +0530

    Very Minor Changes

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:283d5aced4de71501724b6f34fe093bd5a0bf5670e0fd6909bb671a316c86ac9
-size 415
+oid sha256:3329bf3920f6fa142e40613dc838bbc840a9dbb046785185dba130e36b2a7f33
+size 1067
