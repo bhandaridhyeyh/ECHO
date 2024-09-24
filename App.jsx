968aeec93f9e9bd991b4d9c406082451c1f3de80
vidit16sh <sh.vidit16@outlook.com>
2024-09-24 10:19:21 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -22,28 +22,3 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   }
 }) 
-/* 
-const firebaseConfig = {
-  // Replace with your Firebase project configuration
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "trademate-881d0",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "1:691241778318:android:420e77377beb1995e11ddf",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const App = () => {
-  return (
-    <View>
-      <Text>Firebase is connected!</Text>
-    </View>
-  );
-};
-
-export default App;
-*/
